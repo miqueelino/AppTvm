c0933c47f336777f81147b9f92f2a77589e600f6
miqueelino <jpaulo.nunez@gmail.com>
2026-03-18 16:28:35 -0300

    Primeiro commit do projeto AppTvm

---
+++ src/main/java/br/com/mique/AppTvm/AppTvmApplication.java
@@ -0,0 +1,13 @@
+package br.com.mique.AppTvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mique/AppTvm/AppTvmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mique.AppTvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
